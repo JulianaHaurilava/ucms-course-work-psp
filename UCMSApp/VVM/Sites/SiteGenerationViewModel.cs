2c2edb470f1bbcc19928782627866ed03b55f60c
JulianaHaurilava <jrediss.k@gmail.com>
2023-12-03 14:43:05 +0300

    site editing

--- UCMSApp/VVM/Sites/SiteGenerationViewModel.cs
+++ UCMSApp/VVM/Sites/SiteGenerationViewModel.cs
@@ -1,13 +1,52 @@
-﻿using UCMSApp.VVM.Base;
+﻿using CMSLib.Enum;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using UCMSApp.Services;
+using UCMSApp.VVM.Base;
 
 namespace UCMSApp.VVM.Sites
 {
+    [QueryProperty("Site", "Site")]
     public partial class SiteGenerationViewModel : BaseViewModel
     {
-        public SiteGenerationViewModel(/*SiteService service*/)
+        [ObservableProperty] private CMSLib.DTO.Site site;
+
+        private SiteService service;
+        public SiteGenerationViewModel(SiteService service)
         {
-            Title = "Генерация сайта";
-            //this.service = service;
+            Title = "Редактирование сайта";
+            this.service = service;
+        }
+
+        [RelayCommand]
+        private async Task UpsertSiteAsync()
+        {
+            if (IsBusy) return;
+
+            try
+            {
+                IsBusy = true;
+                var response = await service.UpsertAsync(Site);
+
+                if (response.Type == ResponseTypes.Ok)
+                {
+                    await Shell.Current.DisplayAlert("Внимание!", response.Message, "Хорошо");
+                    await Shell.Current.Navigation.PopAsync(true);
+                }
+                else
+                {
+                    await Shell.Current.DisplayAlert("Ошибка!", response.Message, "Хорошо");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                await Shell.Current.DisplayAlert("Ошибка!", ex.Message, "Хорошо");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
         }
     }
 }
