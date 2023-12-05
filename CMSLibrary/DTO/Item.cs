86cdba4ed975478c7fcc2baca585e6fb5aa25fc2
JulianaHaurilava <jrediss.k@gmail.com>
2023-12-05 16:20:58 +0300

    smaller items

--- CMSLibrary/DTO/Item.cs
+++ CMSLibrary/DTO/Item.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-        public double Price { get; set; } = double.MinValue;
+        public double Price { get; set; } = 0.0;
         public int SiteId { get; set; }
         public required Site Site { get; set; }
     }
--- CMSLibrary/DTO/Site.cs
+++ CMSLibrary/DTO/Site.cs
@@ -5,7 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-        public string Url { get; set; } = string.Empty;
 
         public int CompanyId { get; set; }
         public required Company Company { get; set; }
